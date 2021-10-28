b5567524a96a9811cefeda770afd14448a0c4830
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-10-27 20:34:41 -0500

    created server.js file

---
+++ server.js
@@ -0,0 +1,56 @@
+// const { response } = require("express");
+// const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+// const { allowedNodeEnvironmentFlags } = require("process");
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+function answers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "what would you like to do?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add a employee",
+          "update an employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then((response) => {
+      switch (response.choices) {
+        case "view all departments":
+          getAllDepartments();
+          break;
+        case "view all roles":
+          getAllRoles();
+          break;
+        case "view all employees":
+          getAllEmployees();
+          break;
+        case "add a department":
+          addDepartment();
+          break;
+        case "add a role":
+          addRole();
+          break;
+        case "add an employee":
+          addEmployee();
+          break;
+        case "update an employee role":
+          updateEmployee();
+          break;
+        default:
+          console.log("SOMETHING WENT WRONG!");
+      }
+    });
+}
