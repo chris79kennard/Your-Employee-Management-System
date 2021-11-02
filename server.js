a2c4511437b21b6fb2cc37a7004de048b4bfe580
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-01 20:20:06 -0500

    fixed add a new employee function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 const util = require("util");
 const { Table } = require("console-table-printer");
 const inquirer = require("inquirer");
+const { start } = require("repl");
 
 // connect to database
 const connection = mysql.createConnection(
@@ -105,5 +106,110 @@ async function getAllEmployees() {
     answers();
   }
 }
+async function addDepartment(){
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          name: "departmentName",
+          message: "What is the name of the new department you would like to add?"
+      }
+    ])
+    .then((response) => {
+        addDepartmentQuery(response);
+    })
+};
 
+async function addDepartmentQuery (res){
+  const {departmentName} = res;
+  try{
+    await query (`INSERT INTO departments (department_name) VALUES (?);`, [departmentName]);
+    const rows = await query(`SELECT * FROM departments;`);
+    consoleTable(rows);
+  } finally {
+    answers();
+  }
+};
+
+async function addRole(){
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          name: "NewRole",
+          message: "What is the name of the new role you would like to add?"
+      },
+      {
+        type: "input",
+        name: "RoleSalary",
+        message: "What is the salary of the new role you would like to add?"
+    },
+    {
+      type: "input",
+      name: "RoleDepartment",
+      message: "What is the department of the new role you would like to add?"
+  }
+    ])
+    .then((response) => {
+        addRoleQuery(response);
+    })
+};
+
+async function addRoleQuery (res){
+  const {NewRole, RoleSalary, RoleDepartment} = res;
+  const deptIdArr = await query(`SELECT id FROM departments WHERE department_name=?;`,[RoleDepartment]);
+  console.log(deptIdArr);
+  const deptId= deptIdArr[0].id;
+  try{
+    await query (`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`, [NewRole, RoleSalary, deptId]);
+    const rows = await query(`SELECT r.id, title, salary, d.department_name AS department FROM roles r JOIN departments d ON r.department_id = d.id;`);
+    consoleTable(rows);
+  } finally {
+    answers();
+  }
+};
+
+
+async function addEmployee(){
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          name: "First_Name",
+          message: "What is the first name of the new employee you would like to add?"
+      },
+      {
+        type: "input",
+        name: "Last_name",
+        message: "What is the last name of the new employee you would like to add?"
+    },
+    {
+      type: "input",
+      name: "employeeRole",
+      message: "What is the role of the new employee you would like to add?"
+  },
+  {
+    type: "input",
+    name: "employeeManager",
+    message: "What is the first name of the new employees manager?"
+}
+    ])
+    .then((response) => {
+        addEmployeeQuery(response);
+    })
+};
+
+async function addEmployeeQuery (res){
+  const {First_Name, Last_name, employeeRole, employeeManager} = res;
+  const role_id = await query(`SELECT id FROM roles WHERE title = ?;`, [employeeRole])
+  const manager_id = await query(`SELECT id FROM employees WHERE first_name = ?;`, [employeeManager])
+
+  try{
+    await query (`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ? );`, [First_Name, Last_name, role_id, manager_id]);
+    const rows = await query(`SELECT e.id, e.first_name, e.last_name, r.title AS role, d.department_name AS department, r.salary, m.last_name AS manager FROM employees e LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN departments d ON r.department_id = d.id LEFT JOIN employees m ON e.manager_id = m.id;`);
+    consoleTable(rows);
+  } finally {
+    answers();
+  }
+};
 answers();
